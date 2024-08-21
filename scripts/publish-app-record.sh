0c4d260744f8da4a875121b7deaaca5b459eedaa
Tran Hong Hai <haitranhong145@gmail.com>
2024-08-22 00:19:46 +0900

    Update scripts

--- scripts/publish-app-record.sh
+++ scripts/publish-app-record.sh
@@ -67,8 +67,10 @@ if [ -z "$CERC_REGISTRY_APP_CRN" ]; then
   CERC_REGISTRY_APP_CRN="lrn://$authority/applications/$app"
   laconic -c $CONFIG_FILE registry authority reserve ${authority} --user-key "${CERC_REGISTRY_USER_KEY}"
   AUCTION_ID=$(laconic -c $CONFIG_FILE registry autority whois ${authority} | jq -r '.[] | .auction | .id')
+  echo 'AUCTION_ID'
   echo $AUCTION_ID
   REVEAL_FILE=$(laconic -c $CONFIG_FILE registry auction bid commit $AUCTION_ID 5000000 alnt | jq -r '.reveal_file')
+  echo 'REVEAL_FILE'
   echo $REVEAL_FILE
   laconic -c $CONFIG_FILE registry auction bid reveal $AUCTION_ID $REVEAL_FILE
   laconic -c $CONFIG_FILE registry auction get $AUCTION_ID
