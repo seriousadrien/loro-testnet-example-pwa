cc1b3ca0caa425317ca6c9dca03e48d049a55c0e
Tran Hong Hai <haitranhong145@gmail.com>
2024-08-22 00:51:43 +0900

    Add the bid step

--- scripts/publish-app-record.sh
+++ scripts/publish-app-record.sh
@@ -67,7 +67,7 @@ if [ -z "$CERC_REGISTRY_APP_CRN" ]; then
   CERC_REGISTRY_APP_CRN="lrn://$authority/applications/$app"
   laconic -c $CONFIG_FILE registry authority reserve ${authority} --user-key "${CERC_REGISTRY_USER_KEY}"
   AUCTION_ID=$(laconic -c $CONFIG_FILE registry authority whois ${authority} | jq -r '.[] | .auction | .id')
-  # REVEAL_FILE=$(laconic -c $CONFIG_FILE registry auction bid commit $AUCTION_ID 5000000 alnt --user-key "${CERC_REGISTRY_USER_KEY}" | jq -r '.reveal_file')
+  REVEAL_FILE=$(laconic -c $CONFIG_FILE registry auction bid commit $AUCTION_ID 5000000 alnt --user-key "${CERC_REGISTRY_USER_KEY}" | jq -r '.reveal_file')
   # laconic -c $CONFIG_FILE registry auction bid reveal $AUCTION_ID $REVEAL_FILE --user-key "${CERC_REGISTRY_USER_KEY}"
   laconic -c $CONFIG_FILE registry auction get $AUCTION_ID --user-key "${CERC_REGISTRY_USER_KEY}"
   laconic -c $CONFIG_FILE registry authority bond set ${authority} ${CERC_REGISTRY_BOND_ID} --user-key "${CERC_REGISTRY_USER_KEY}"
