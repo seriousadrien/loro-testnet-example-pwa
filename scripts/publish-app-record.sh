76f079d2091b6563421abb6dfed727234a6af463
Tran Hong Hai <haitranhong145@gmail.com>
2024-08-22 10:31:17 +0900

    Skip registering authority after bid

--- scripts/publish-app-record.sh
+++ scripts/publish-app-record.sh
@@ -65,7 +65,7 @@ if [ -z "$CERC_REGISTRY_APP_CRN" ]; then
   authority=$(echo "$rcd_name" | cut -d'/' -f1 | sed 's/@//')
   app=$(echo "$rcd_name" | cut -d'/' -f2-)
   CERC_REGISTRY_APP_CRN="lrn://$authority/applications/$app"
-  laconic -c $CONFIG_FILE registry authority reserve ${authority} --user-key "${CERC_REGISTRY_USER_KEY}"
+  # laconic -c $CONFIG_FILE registry authority reserve ${authority} --user-key "${CERC_REGISTRY_USER_KEY}"
   AUCTION_ID=$(laconic -c $CONFIG_FILE registry authority whois ${authority} | jq -r '.[] | .auction | .id')
   REVEAL_FILE=$(laconic -c $CONFIG_FILE registry auction bid commit $AUCTION_ID 5000000 alnt --user-key "${CERC_REGISTRY_USER_KEY}" | jq -r '.reveal_file')
   laconic -c $CONFIG_FILE registry auction bid reveal $AUCTION_ID $REVEAL_FILE --user-key "${CERC_REGISTRY_USER_KEY}"
